6eb02ae472f5433e6d9c1fd350e2e7912d457b15
adilsonxdesouza <adilsonxdesouza@gmail.com>
2021-12-30 02:07:41 -0300

    first

---
+++ src/index.js
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+require('file-loader?name=[name].[ext]!./index.html'); // a ausência desta linha gera o erro de directory listing (mostrar o diretório ao invez da página index.html)
+
+
+ReactDOM.render(
+    <>
+        ola mundo
+    </>,
+    document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+﻿const path = require("path")
+
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3050,
+    watchContentBase: true,
+   // historyApiFallback: true, // corrige o erro cannot get pagina do  <Switch> < Route path="/pag1" component={ Pag1 } /></Swith>
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      ///////
+      // As 2  regras abaixo servem para difenciar os arquivos css / scss normais dos arquivos usados em modulos css no react
+      // Como o webpack exige que os arquivos css usados para módulos tenha como nome algumacoisa.module.css esta exigência
+      // é aproveitada nos arquivos abaixo : A primeira regra ( sem módulo ) usa o " exclude: /\.module\.css$/ " e a 
+      // segunda regra (com módulo ) usa o  "include: /\.module\.css$/" . Sem estes elementos include e exclude como estão
+      // Abaixo é gerado erro na compilação pelo webpack.
+      // Note que não foi necessário instalar nenhum plugin adicional para usar o module css
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          // MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+        exclude: /\.module\.css$/
+      },
+      //////
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+            }
+          }
+        ],
+        include: /\.module\.css$/
+      },
+      //////
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[hash].[ext]',
+        },
+      },
+
+    ]
+  }
+}
