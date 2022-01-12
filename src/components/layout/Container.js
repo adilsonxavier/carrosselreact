155940d7269c41e71b336c8e4e3cd03f50cc876a
adilsonxavier <adilsonxdesouza@gmail.com>
2022-01-12 13:27:32 -0300

    aula26

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -3,8 +3,8 @@ import styles from "./Container.module.css";
 
 export default function Container(props) {
     return (
-        /* <div id="divcont" className={`${styles.Container} ${styles[props.customClass]}` }>*/
-        <div className={`${styles.Container}`}>
+         <div id="divcont" className={`${styles.Container} ${styles[props.customClass]}` }>
+ 
             {props.children}
         </div>
         );
