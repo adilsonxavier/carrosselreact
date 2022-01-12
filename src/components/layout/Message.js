95fa73b35b2e3b757029ccf474ce5633e3b5f92f
adilsonxavier <adilsonxdesouza@gmail.com>
2022-01-12 12:27:26 -0300

    aula25_message

---
+++ src/components/layout/Message.js
@@ -0,0 +1,35 @@
+﻿import React from "react";
+import styles from "./Message.module.css";
+
+
+
+
+export default function Message({ type, msg }) {  // Ao invez de props posso passar tanbem um objeto com parâmetros
+// export default function Message(props) {
+
+    const [visible, setVisible] = React.useState(false);
+
+    React.useEffect(() => {
+        if (!msg) {
+            setVisible(false);
+            return;
+        }
+        setVisible(true);
+
+        const timer = setTimeout(() => { setVisible(false) },
+            3000);
+
+        return () => clearTimeout(timer); // esta linha comentada não mudou o comportamento - ver melhor posteriormente
+
+    }, [msg])
+
+    return (
+
+        <> {visible &&
+            (<div className={`${styles.message} ${styles[type]}`}> {msg}</div>)
+        }
+        </>
+
+    );
+
+}
