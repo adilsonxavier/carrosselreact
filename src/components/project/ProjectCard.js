402f2df944b1d8c875da02eeb4b4e8d8ed6d54e3
adilsonxavier <adilsonxdesouza@gmail.com>
2022-01-12 21:44:27 -0300

    aula27

---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import styles from "./ProjectCard.module.css";
+import { BsPencil, BsFillTrashFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+export default function ProjectCard({ id,name,budget,category, handleRemove}) {
+
+   // className = {`${styles[]}`}
+
+    return (
+        <div className={styles.project_card}>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento:</span>&nbsp;R${budget}
+            </p>
+            <p className={ styles.category_text }>
+                <span className={category ? `${styles[category.toLowerCase()]}` : ''}></span>{category}
+            </p>
+            <div className={styles.project_card_actions}>
+                <Link to="/" ><BsPencil /> editar </Link>
+                <button><BsFillTrashFill/> Remover</button>
+           </div>
+        </div>
+    );
+
+}
